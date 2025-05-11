008bad2d62db8e0f50bd20ef7698c710e8406040
Oleg <finkton23@gmail.com>
2025-05-11 18:59:10 +0500

    Update Task5V.cs FINAL

--- HomeWorkNumb2/Task5V.cs
+++ HomeWorkNumb2/Task5V.cs
@@ -7,10 +7,10 @@ internal class Task5X
         const int EuroDollarDiff = 14;
         const int EuroRubleDiff = 46;
         const int DollarRubleDiff = 83;
-        int totalByCurrency = 0;
         int ruble = 1000;
         int dollar = 1000;
         int euro = 1000;
+        int toConvert = 0;
         Console.WriteLine("Добро пожаловать в обменник валют, чтобы продолжить, нажмите \"Enter\". Для выхода из программы писать - \"Exit\" ");
         while (true)
         {
@@ -33,41 +33,93 @@ internal class Task5X
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
                 switch (keyInfo.Key)
                 {
-                    case ConsoleKey.D1:
-                        totalByCurrency = ruble / DollarRubleDiff;
+                    case ConsoleKey.D1: // Скопировал твою подсказку, и модифицировал её
                         Console.WriteLine(" ");
-                        Console.WriteLine($"У вас {totalByCurrency} долларов...");
-                        Console.WriteLine("Для повтора, жать \"Enter\".");
+                        Console.WriteLine("Введите, сколько рублей хотите конвертировать...");
+                        toConvert = int.Parse(Console.ReadLine());
+                        if (toConvert > 0)
+                        {
+                            ruble -= toConvert; // с баланса рублей вычитаем введённое с консоли колво рублей для перевода ТВОЕ
+                            int dollarDelta = toConvert / DollarRubleDiff; // вычисляем сколько из введённых рублей получится долларов ТВОЕ
+                            dollar +=  dollarDelta; // прибавляем к доллар счёту полученные из рублей доллары ТВОЕ
+                            Console.WriteLine(" ");
+                            Console.WriteLine($"У вас {dollar} долларов...");
+                            Console.WriteLine("Чтобы продолжить, нажмите \"Enter\"");
+                        }
                         break;
                     case ConsoleKey.D2:
-                        totalByCurrency = ruble / EuroRubleDiff;
                         Console.WriteLine(" ");
-                        Console.WriteLine($"У вас {totalByCurrency} евро...");
-                        Console.WriteLine("Для повтора, жать \"Enter\".");
+                        Console.WriteLine("Введите, сколько рублей хотите конвертировать...");
+                        toConvert = int.Parse(Console.ReadLine());
+                        if (toConvert > 0)
+                        {
+                            ruble -= toConvert;
+                            int euroDelta = toConvert / EuroRubleDiff;
+                            euro += euroDelta;
+                            Console.WriteLine(" ");
+                            Console.WriteLine($"У вас {euro} евро...");
+                            Console.WriteLine("Для повтора, жать \"Enter\".");
+                        }
                         break;
                     case ConsoleKey.D3:
-                        totalByCurrency = dollar / DollarRubleDiff;
                         Console.WriteLine(" ");
-                        Console.WriteLine($"У вас {totalByCurrency} рублей...");
-                        Console.WriteLine("Для повтора, жать \"Enter\".");
+                        Console.WriteLine("Введите, сколько долларов хотите конвертировать...");
+                        toConvert = int.Parse(Console.ReadLine());
+                        if (toConvert > 0)
+                        {
+                            dollar -= toConvert;
+                            int rubleDelta = toConvert / DollarRubleDiff;
+                            ruble += rubleDelta;
+                            Console.WriteLine(" ");
+                            Console.WriteLine($"У вас {ruble} рублей...");
+                            Console.WriteLine("Для повтора, жать \"Enter\".");
+                        }
+
                         break;
                     case ConsoleKey.D4:
-                        totalByCurrency = dollar / EuroDollarDiff;
                         Console.WriteLine(" ");
-                        Console.WriteLine($"У вас {totalByCurrency} евро...");
-                        Console.WriteLine("Для повтора, жать \"Enter\".");
+                        Console.WriteLine("Введите, сколько долларов хотите конвертировать...");
+                        toConvert = int.Parse(Console.ReadLine());
+                        if (toConvert > 0)
+                        {
+                            dollar -= toConvert;
+                            int euroDelta = toConvert / EuroDollarDiff;
+                            euro += euroDelta;
+                            Console.WriteLine(" ");
+                            Console.WriteLine($"У вас {euro} евро...");
+                            Console.WriteLine("Для повтора, жать \"Enter\".");
+                        }
+
                         break;
                     case ConsoleKey.D5:
-                        totalByCurrency = euro / EuroDollarDiff;
                         Console.WriteLine(" ");
-                        Console.WriteLine($"У вас {totalByCurrency} долларов...");
-                        Console.WriteLine("Для повтора, жать \"Enter\".");
+                        Console.WriteLine("Введите, сколько евро хотите конвертировать...");
+                        toConvert = int.Parse(Console.ReadLine());
+                        if (toConvert > 0)
+                        {
+                            euro -= toConvert;
+                            int dollarDelta = toConvert / EuroDollarDiff;
+                            dollar += dollarDelta;
+                            Console.WriteLine(" ");
+                            Console.WriteLine($"У вас {dollar} долларов...");
+                            Console.WriteLine("Для повтора, жать \"Enter\".");
+                        }
+
                         break;
                     case ConsoleKey.D6:
-                        totalByCurrency = euro / EuroRubleDiff;
                         Console.WriteLine(" ");
-                        Console.WriteLine($"У вас {totalByCurrency} рублей...");
-                        Console.WriteLine("Для повтора, жать \"Enter\".");
+                        Console.WriteLine("Введите, сколько евро хотите конвертировать...");
+                        toConvert = int.Parse(Console.ReadLine());
+                        if (toConvert > 0)
+                        {
+                            euro -= toConvert;
+                            int rubleDelta = toConvert / EuroRubleDiff;
+                            ruble += rubleDelta;
+                            Console.WriteLine(" ");
+                            Console.WriteLine($"У вас {ruble} рублей...");
+                            Console.WriteLine("Для повтора, жать \"Enter\".");
+                            break;
+                        }
                         break;
                 }
             }
